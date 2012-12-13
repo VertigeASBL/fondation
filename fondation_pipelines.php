685132409763c9fd3e54e11c68b4f00b92752225
bystrano <bystrano@gmx.ch>
2012-12-13 12:29:18 +0100

    debogue le commit précédent : jQuery se charge correctement dans l'espace privé

--- fondation_pipelines.php
+++ fondation_pipelines.php
@@ -2,8 +2,11 @@
 
 function fondation_jquery_plugins ($scripts) {
 
-  // virer le jquery de spip, on s'en occupe dans inclure/insert_head.html
-  array_shift($scripts);
+  // virer le jquery de spip, qu'on remplace par celui de Foundation, qui est plus
+  // récent et minifié.
+  $jquery_location = find_in_path('javascripts/jquery.min.js');
+  $jquery_location = preg_replace('#^../#', '', $jquery_location);
+  $scripts[0] = $jquery_location;
 
   return $scripts;
 }
