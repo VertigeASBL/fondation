fb30757174786968b54c7cb29882b6d51db0efe9
bystrano <bystrano@gmx.ch>
2013-01-12 18:08:00 +0100

    adapte le chemin des fichiers js

--- fondation_pipelines.php
+++ fondation_pipelines.php
@@ -4,7 +4,7 @@ function fondation_jquery_plugins ($scripts) {
 
   // virer le jquery de spip, qu'on remplace par celui de Foundation, qui est plus
   // récent et minifié.
-  $jquery_location = find_in_path('javascripts/jquery.min.js');
+  $jquery_location = find_in_path('javascripts/foundation/jquery.js');
   $jquery_location = preg_replace('#^../#', '', $jquery_location);
   $scripts[0] = $jquery_location;
 
