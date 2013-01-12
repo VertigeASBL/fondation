7cfb9427821f70e090aab730288ab770efd082a6
bystrano <bystrano@gmx.ch>
2013-01-12 16:50:17 +0100

    adapte les descriptions du plug

--- lang/paquet-foundation_fr.php
+++ lang/paquet-foundation_fr.php
@@ -6,8 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'foundation_description' => 'Intégration du framework Foundation dans les squelettes ZPIP.',
-	'foundation_nom' => 'Framework Foundation pour Z',
+	'foundation_description' => 'Int&eacute;gration du framework Foundation 3 dans les squelettes ZPIP. Utilise la version du framework que l\'on g&eacut;nere soi-meme avec compass.',
+	'foundation_nom' => 'Framework Foundation 3 pour Z',
 	'foundation_slogan' => '',
 );
 
